8aaf299f5c1b390a65b659ca096c3042e212af16
Ibrahim <mamedovarbeiter@gmail.com>
2022-10-28 22:49:16 +0300

    fibonacci numbers

---
+++ Fibonacci.cs
@@ -0,0 +1,58 @@
+namespace Fibonacci
+{
+    public class Fibonacci
+    {
+        public int GetSmallFibonacciNumber(long fibNumber)
+        {
+            int fib1 = 0;
+            int fib2 = 1;
+            int fib3 = 0;
+
+            for (int i = 0; i < fibNumber - 1; i++)
+            {
+                fib3 = fib2;
+                fib2 = fib1 + fib2;
+                fib1 = fib3;
+            }
+
+            return fib2;
+        }
+        public int LastDigitOfTheBigFibonacciNumber(long fibNumber)
+        {
+            int fib1 = 0;
+            int fib2 = 1;
+            int fib3 = 0;
+
+            for (int i = 0; i < fibNumber - 1; i++)
+            {
+                fib3 = fib2;
+                fib2 = fib1 + fib2 % 10;
+                fib1 = fib3;
+            }
+
+            return fib2 % 10;
+        }
+        public int HugeFibonacciNumberModulo(long fibNumber, int fibModulo)
+        {
+
+            int[] arrFib = new int[fibModulo * 6];
+            int length = 0;
+
+            arrFib[0] = 0;
+            arrFib[1] = 1;
+            
+            for (int i = 2; i < 6 * fibModulo; i++)
+            {
+                arrFib[i] = (arrFib[i - 1] + arrFib[i - 2]) % fibModulo;
+                
+                if (arrFib[i] == 1 && arrFib[i - 1] == 0)
+                {
+                    length = i - 1;
+                    break;
+                }
+            }
+
+            return arrFib[fibNumber % length];
+        }
+    }
+}        
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type numbers");
+            
+            long fibNumber = Convert.ToInt64(Console.ReadLine());
+            
+            int fibModulo = Convert.ToInt32(Console.ReadLine());
+            
+            Fibonacci.Fibonacci getFibonacciNumber = new Fibonacci.Fibonacci();
+            
+            Console.WriteLine();
+            Console.WriteLine("Small Fibonacci Number");
+            Console.WriteLine(getFibonacciNumber.GetSmallFibonacciNumber(fibNumber));
+            
+            Console.WriteLine();
+            Console.WriteLine("Last Digit Of The Big Fibonacci Number");
+            Console.WriteLine(getFibonacciNumber.LastDigitOfTheBigFibonacciNumber(fibNumber));
+            
+            Console.WriteLine();
+            Console.WriteLine("Huge Fibonacci Number Modul");
+            Console.WriteLine(getFibonacciNumber.HugeFibonacciNumberModulo(fibNumber, fibModulo));
+        }
+    }
+}
